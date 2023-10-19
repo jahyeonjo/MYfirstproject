b59d01978f2afc4e22fb7ead57b2251ae38fd440
jahyeon <147809141+jahyeonjo@users.noreply.github.com>
2023-10-20 02:02:50 +0800

    Add files via upload

---
+++ CS50P/deepthoughts.py
@@ -0,0 +1,7 @@
+answer = input("What is the Answer to the Great Question of Life, The Universe, and Everything? ").lower().replace("-", " ")
+
+
+if answer == "42" or answer == "forty two":
+    print("Yes")
+else:
+    print("No")
